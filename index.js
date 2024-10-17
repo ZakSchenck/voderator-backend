dbcec0d1292e593e3f95b3f385b08fb107e6fa1a
Zak Schenck <zakschenck@Zaks-MBP-2.attlocal.net>
2024-10-17 02:20:06 -0400

    configured for vercel

--- index.js
+++ index.js
@@ -1,29 +1,26 @@
+
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
-const userRoutes = require('./routes/userRoutes.js');
-const cors = require('cors')
+const userRoutes = require('../routes/userRoutes.js');
+const cors = require('cors');
 
 dotenv.config();
 
 app.use(express.json());
 
 const corsOptions = {
-    origin: 'http://localhost:5173', // Allow only this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow these methods
-    credentials: true // Allow cookies to be sent (if needed)
-  };
-  
-  app.use(cors(corsOptions));
+    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+};
 
-const PORT = process.env.PORT || 3000;
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Express JWT Auth API!');
+    res.send('Welcome to the Express JWT Auth API!');
 });
 
 app.use('/api/v1', userRoutes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+module.exports = app;
